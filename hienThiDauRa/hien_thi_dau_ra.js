e64833d1ea5bd57f356e83de750f31c582a2980a
Zakhanhrog <18khanh.2003@gmail.com>
2025-03-09 17:39:58 +0700

    hienThiDauRafile

---
+++ hienThiDauRa/hien_thi_dau_ra.js
@@ -0,0 +1,13 @@
+function vinh() {
+    alert("xin chao")
+}
+function vinh1() {
+    document.getElementById("vinhngo1").innerHTML="xin chao"
+}
+function vinh2(){
+
+    document.write("xin chao")
+}
+function vinh3(){
+    console.log("xin chao")
+}
